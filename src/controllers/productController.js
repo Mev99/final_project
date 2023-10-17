431fab430afdb91b4e9b4965058bd0b00fc61c0a
Matias Ayesa <matiasayesa99@outlook.com>
2023-10-16 23:29:55 -0300

    some layers done

---
+++ src/controllers/productController.js
@@ -0,0 +1,32 @@
+import productModel from "../models/product.model.js"
+
+async function getProducts(req, res) {
+    try {
+        let limitParam = req.params.limit || 10
+        let pageParam = req.params.page || 1
+        let sortParam = req.params.sort || 0
+        let queryUrl = req.query || null
+    
+        const findProducts = await productModel.paginate(queryUrl, { limit: limitParam, page: pageParam, sort: { price: sortParam } })
+    
+        res.send({ status: 'success', payload: findProducts })
+    } catch (error) {
+        console.log(error)
+    }
+
+}
+
+async function updateMany(req, res) {
+    try {
+        const queryId = req.query
+        const data = req.body
+
+        const updateAll = await productModel.updateMany(queryId, data)
+
+        res.send({ result: "success", payload: updateAll })
+    } catch (error) {
+        console.log(error)
+    }
+}
+
+export default {getProducts, updateMany}
--- src/router/product.router.js
+++ src/router/product.router.js
@@ -1,31 +1,11 @@
 import { Router } from "express";
-import productModel from "../models/product.model.js";
+import productController from "../controllers/productController.js";
+
 const productRouter = Router()
 
-productRouter.get("/:limit?/:page?/:sort?/:query?", async (req, res) => {
-    let limitParam = req.params.limit || 10
-    let pageParam = req.params.page || 1
-    let sortParam = req.params.sort || 0
-    let queryUrl = req.query || null
+productRouter.get("/:limit?/:page?/:sort?/:query?", productController.getProducts)
 
-    const findProducts = await productModel.paginate(queryUrl, { limit: limitParam, page: pageParam, sort: { price: sortParam } })
-
-    //TO DO: prevLink & nextLink: links a la pagina anterior y posterior respectivamente
-    res.send({ status: 'success', payload: findProducts })
-})
-
-productRouter.put('/:uid?', async (req, res) => {
-    try {
-        const queryId = req.query
-        const data = req.body
-
-        const updateAll = await productModel.updateMany(queryId, data)
-
-        res.send({ result: "success", payload: updateAll })
-    } catch (error) {
-        console.log(error)
-    }
-})
+productRouter.put('/:uid?', productController.updateMany)
 
 
 export default productRouter
